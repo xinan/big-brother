8e11cad38e85d6dced91b8c4ff8f892001b07fb7
Bili Xu <xubili93@gmail.com>
2015-03-15 06:39:47 +0800

    remove cors

--- app.js
+++ app.js
@@ -16,13 +16,7 @@ var io = require('socket.io')(server);
 var routes = require('./routes/index')(io);
 var users = require('./routes/users');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
-}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(allowCrossDomain);
+
 
 app.use('/', routes);
 app.use('/users', users);
