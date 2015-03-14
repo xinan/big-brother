43bc859504ac2ccfcd37ce32ce44811da2b88b66
Varun Patro <varun.kumar.patro@gmail.com>
2015-03-15 01:29:13 +0800

    add a comma after id

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ function sendOffer(socket, report) {
     reader.readAsDataURL(imageFile);
     */
     var offer = {
-        id: 1
+        id: 1,
         image: 'burgerking',
         title: 'Burger King Chicken Royale',
         tier: 2,
