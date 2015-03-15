167faf9335cf6d00b41a37f5979f0c3f8c182b00
Bili Xu <xubili93@gmail.com>
2015-03-15 08:20:21 +0800

    Emit register user

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     });
   });
 
-  $('#emitRegister').on('click', function() {
+  $('#emitRegisterUser').on('click', function() {
     console.log('Emit register clicked');
     $.ajax({
       method: 'GET',
