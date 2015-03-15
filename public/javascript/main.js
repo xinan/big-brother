c12b7d4f03952cd3eaf4eca28cd3dd96829a07a6
Bili Xu <xubili93@gmail.com>
2015-03-15 09:22:52 +0800

    Remove io.emit

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
 
   $('#offer1').on('click', function() {
     console.log('Offer 1 sent');
-    /*
     $.ajax({
       method: 'GET',
       url: '/sendoffer1'
     });
-    */
-    io.emit('send offer', 'burgerking');
   });
 
   $('#offer2').on('click', function() {
