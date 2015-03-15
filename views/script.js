5a72e0116a414002a227b7b698b6522c786319cf
Bili Xu <xubili93@gmail.com>
2015-03-15 08:40:34 +0800

    Change localhost to heroku

--- views/script.js
+++ views/script.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000');
+var socket = io('http://big-brother-api.herokuapp.com');
   socket.on('CONNECTION_STARTED', function (data) {
     console.log(data);
     socket.emit('CONNECTION_CONFIRMED', "Client connected successfully.");
