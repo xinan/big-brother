4d4b351a5a2fbe4ee10f67586460ef06b53a081b
Bili Xu <xubili93@gmail.com>
2015-03-15 08:23:34 +0800

    Move patrick

--- app.js
+++ app.js
@@ -183,8 +183,8 @@ io.on('connection', function(socket) {
               flightTime: "2015-03-15T13:00:00.000+08:00"
             },
             location: {
-              lat: 25.244415300668223,
-              long: 55.37209699963714,
+              lat: 25.244515300668223,
+              long: 55.37199699963714,
               floor: 1
             },
             boardingGate: 'A1',
@@ -273,65 +273,6 @@ server.listen(port, function() {
 //     });
 //     socket.emit("late person");
 // });
-
-/*
-// Returns the idlers 
-app.get('/idlers', function(req, res) {
-  res.send({
-    users: [
-      {
-        deviceId: 1,
-        name: "Lu Bili",
-        flight: {
-          flightNo: "EK01",
-          flightTime: "2015-04-23T20:00:00.000+08:00"
-        },
-        location: {
-          lat: 25.244515300668223,
-          long: 55.37045352788945,
-          floor: 1
-        },
-        boardingGate: 'A1',
-        status: 'agent',
-        statusTime: 12
-      },
-      {
-        deviceId: 2,
-        name: "Adola Fazli",
-        flight: {
-          flightNo: "EK02",
-          flightTime: "2015-04-23T20:00:00.000+08:00"
-        },
-        location: {
-          lat: 25.24282729711983,
-          long: 55.37245461207174,
-          floor: 1
-        },
-        boardingGate: 'A1',
-        status: 'idle',
-        statusTime: 23
-      },
-      {
-        deviceId: 3,
-        name: "Mrunal Kumar",
-        flight: {
-          flightNo: "EK03",
-          flightTime: "2015-04-23T20:00:00.000+08:00"
-        },
-        location: {
-          lat: 25.243092514556807,
-          long: 55.37219699963714,
-          floor: 1
-        },
-        boardingGate: 'A1',
-        status: 'alert',
-        statusTime: 10
-      }
-    ],
-    requestTime: "2015-04-23T20:00:00.000+08:00"
-  });
-});
-*/
 
 /**
 * Helper functions
