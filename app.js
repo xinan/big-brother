b125316d342dc4a7c302b9b373d284ee56b43f12
Bili Xu <xubili93@gmail.com>
2015-03-15 08:11:25 +0800

    Edited dummy data

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ io.on('connection', function(socket) {
         userData.reports.push(report);
         sendOffer(socket, report);
     });
-    
+
     socket.on('register', function (req) {
         socket.emit('register user', '');
     });
@@ -159,22 +159,6 @@ io.on('connection', function(socket) {
             statusTime: 10
           },
           {
-            deviceId: 4,
-            name: "Spongebob Squarepants",
-            flight: {
-              flightNo: "EK03",
-              flightTime: "2015-03-15T13:00:00.000+08:00"
-            },
-            location: {
-              lat: 25.243192514556807,
-              long: 55.37129699963714,
-              floor: 1
-            },
-            boardingGate: 'A1',
-            status: 'agent',
-            statusTime: 10
-          },
-          {
             deviceId: 5,
             name: "Patrick Star",
             flight: {
@@ -182,8 +166,8 @@ io.on('connection', function(socket) {
               flightTime: "2015-03-15T13:00:00.000+08:00"
             },
             location: {
-              lat: 25.243192514556807,
-              long: 55.37209699963714,
+              lat: 25.243292514556807,
+              long: 55.37219699963714,
               floor: 1
             },
             boardingGate: 'A1',
@@ -239,22 +223,6 @@ io.on('connection', function(socket) {
             statusTime: 10
           },
           {
-            deviceId: 9,
-            name: "Ryan Gosling",
-            flight: {
-              flightNo: "EK03",
-              flightTime: "2015-03-15T13:00:00.000+08:00"
-            },
-            location: {
-              lat: 25.243302514556807,
-              long: 55.37330699963714,
-              floor: 1
-            },
-            boardingGate: 'A1',
-            status: 'idle',
-            statusTime: 10
-          },
-          {
             deviceId: 10,
             name: "Steve Jobs",
             flight: {
@@ -268,38 +236,6 @@ io.on('connection', function(socket) {
             },
             boardingGate: 'A1',
             status: 'alert',
-            statusTime: 10
-          },
-          {
-            deviceId: 11,
-            name: "Mr Pringles",
-            flight: {
-              flightNo: "EK03",
-              flightTime: "2015-03-15T13:00:00.000+08:00"
-            },
-            location: {
-              lat: 25.243292514556807,
-              long: 55.37239699963714,
-              floor: 1
-            },
-            boardingGate: 'A1',
-            status: 'alert',
-            statusTime: 10
-          },
-          {
-            deviceId: 11,
-            name: "LeBron James",
-            flight: {
-              flightNo: "EK03",
-              flightTime: "2015-03-15T13:00:00.000+08:00"
-            },
-            location: {
-              lat: 25.243392514556807,
-              long: 55.37219699963714,
-              floor: 1
-            },
-            boardingGate: 'A1',
-            status: 'idle',
             statusTime: 10
           }];
         socket.emit('all users', userCollection);
